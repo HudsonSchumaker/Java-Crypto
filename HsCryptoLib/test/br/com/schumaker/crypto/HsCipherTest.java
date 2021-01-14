6701ec88723c86cdbd0f91dbdc930ffa465fcc0d
HudsonSchumaker <hudson.schumaker@me.com>
2021-01-14 06:12:48 +0100

    Test generate16Key

--- HsCryptoLib/test/br/com/schumaker/crypto/HsCipherTest.java
+++ HsCryptoLib/test/br/com/schumaker/crypto/HsCipherTest.java
@@ -1,9 +1,22 @@
 package br.com.schumaker.crypto;
 
+import junit.framework.Assert;
+import org.junit.Test;
+
 /**
  *
  * @author Hudson Schumaker
  */
 public class HsCipherTest {
     
+    @Test
+    public void testGenerate16Key() {
+        // preparation
+        var tested = new HsCipher();
+        // test
+        var result = tested.generate16Key();
+        // assertion
+        Assert.assertEquals(16, result.length()); 
+    }
+            
 }
