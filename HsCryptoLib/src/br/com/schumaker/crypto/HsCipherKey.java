d73b5e4ec93e97dd99126b9510157941383c3e39
HudsonSchumaker <hudson.schumaker@me.com>
2021-01-14 06:20:33 +0100

    rename

---
+++ HsCryptoLib/src/br/com/schumaker/crypto/HsCipherKey.java
@@ -0,0 +1,45 @@
+package br.com.schumaker.crypto;
+
+import java.util.Random;
+
+/**
+ *
+ * @author Hudson Schumaker
+ */
+public class HsCipherKey {
+
+    private static final char[] CHARS;
+
+    static {
+        CHARS = "@!§$%&()=?{[]}+-<>abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890".toCharArray();
+    }
+
+    public String generate16Key() {
+        return createKey(16);
+    }
+    
+    public String generate32Key() {
+        return createKey(32);
+    }
+    
+    public String generate64Key() {
+        return createKey(64);
+    }
+    
+    public String generate128Key() {
+        return createKey(128);
+    }
+    
+    public String generate256Key() {
+        return createKey(256);
+    }
+
+    private String createKey(final int size) {
+        final Random r = new Random(System.currentTimeMillis());
+        final char[] id = new char[size];
+        for (int i = 0; i < size; ++i) {
+            id[i] = HsCipherKey.CHARS[r.nextInt(HsCipherKey.CHARS.length)];
+        }
+        return new String(id);
+    }
+}
