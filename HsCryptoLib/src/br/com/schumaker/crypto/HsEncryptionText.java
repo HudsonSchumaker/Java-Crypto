2867cbdb1f44ec3b0b99eca29ba068176a258121
HudsonSchumaker <hudson.schumaker@me.com>
2021-01-16 15:40:49 +0100

    StandardCharsets

--- HsCryptoLib/src/br/com/schumaker/crypto/HsEncryptionText.java
+++ HsCryptoLib/src/br/com/schumaker/crypto/HsEncryptionText.java
@@ -4,6 +4,8 @@ import java.nio.charset.StandardCharsets;
 import java.security.InvalidKeyException;
 import java.security.Key;
 import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
+import java.util.Base64;
 import javax.crypto.BadPaddingException;
 import javax.crypto.Cipher;
 import javax.crypto.IllegalBlockSizeException;
@@ -19,24 +21,38 @@ public class HsEncryptionText {
     private static final String ALGORITHM = "AES";
     
     public String encrypt(final String key, final String inputText) {
-        return this.doCrypto(Cipher.ENCRYPT_MODE, key, inputText);
+        return this.doEncrypt(key, inputText);
     }
     
     public String decrypt(final String key, final String inputText) {
-         return this.doCrypto(Cipher.DECRYPT_MODE, key, inputText);
+        return this.doDecrypt(key, inputText);
     }
     
-    private String doCrypto(final int cipherMode, final String key, String inputText) {
+    private String doEncrypt(final String key, String inputText) {
         try {
-            final Key secretKey = new SecretKeySpec(key.getBytes(), ALGORITHM);
+            final Key secretKey = new SecretKeySpec(key.getBytes(StandardCharsets.UTF_8), ALGORITHM);
             final Cipher cipher = Cipher.getInstance(ALGORITHM);
-            cipher.init(cipherMode, secretKey);
-            final byte[] outputBytes = cipher.doFinal(inputText.getBytes());
+            cipher.init(Cipher.ENCRYPT_MODE, secretKey);
+            final byte[] outputBytes = cipher.doFinal(inputText.getBytes(StandardCharsets.UTF_8));
             
-            return new String(outputBytes, StandardCharsets.UTF_8);
+            return Base64.getEncoder().encodeToString(outputBytes);
         } catch (InvalidKeyException | NoSuchAlgorithmException | BadPaddingException 
                 | IllegalBlockSizeException | NoSuchPaddingException ex) {
-            throw new RuntimeException("Encrypt error", ex);
+            throw new RuntimeException("Encrypt Error", ex);
+        }
+    }
+    
+    private String doDecrypt(final String key, String inputText) {
+        try {
+            final Key secretKey = new SecretKeySpec(key.getBytes(StandardCharsets.UTF_8), ALGORITHM);
+            final Cipher cipher = Cipher.getInstance(ALGORITHM);
+            cipher.init(Cipher.DECRYPT_MODE, secretKey);
+            final byte[] outputBytes = cipher.doFinal(Base64.getDecoder().decode(inputText));
+            
+            return new String(outputBytes);
+        } catch (InvalidKeyException | NoSuchAlgorithmException | BadPaddingException 
+                | IllegalBlockSizeException | NoSuchPaddingException ex) {
+            throw new RuntimeException("Decrypt Error", ex);
         }
     }
 }
