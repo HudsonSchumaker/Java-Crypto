6c95aeef0f9f5780435fc096d15178f84898437e
HudsonSchumaker <hudson.schumaker@me.com>
2021-01-16 15:47:52 +0100

    Key secret 256 generation.

--- HsCryptoLib/src/br/com/schumaker/crypto/HsCipherKey.java
+++ HsCryptoLib/src/br/com/schumaker/crypto/HsCipherKey.java
@@ -1,33 +1,40 @@
 package br.com.schumaker.crypto;
 
+import java.nio.charset.StandardCharsets;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.util.Arrays;
+import java.util.Base64;
 import java.util.Random;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
 
 /**
  *
  * @author Hudson Schumaker
  */
 public class HsCipherKey {
-
-    private static final char[] CHARS;
-
-    static {
-        CHARS = "@!§$%&()=?{[]}+-<>abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890".toCharArray();
-    }
-
-    public String generate16Key() {
-        return createKey(16);
+    
+    private static final String ALGORITHM = "AES";
+    
+    public String generate256Key() {
+        return this.createKey();
     }
     
-    public String generate32Key() {
-        return createKey(32);
-    }
-    
-    private String createKey(final int size) {
-        final Random r = new Random(System.currentTimeMillis());
-        final char[] key = new char[size];
-        for (int i = 0; i < size; ++i) {
-            key[i] = HsCipherKey.CHARS[r.nextInt(HsCipherKey.CHARS.length)];
+    private String createKey() {
+        try {
+            KeyGenerator keyGenerator = KeyGenerator.getInstance(ALGORITHM);
+            SecureRandom secureRandom = new SecureRandom();
+            int keyBitSize = 256;
+            keyGenerator.init(keyBitSize, secureRandom);
+            
+            SecretKey secretKey = keyGenerator.generateKey();
+            
+            return Base64.getEncoder().encodeToString(secretKey.getEncoded());
+        } catch (NoSuchAlgorithmException ex) {
+            throw new RuntimeException("Algorithm not supported.");
         }
-        return new String(key);
     }
 }
