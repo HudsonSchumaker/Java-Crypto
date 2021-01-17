b9f66a420358176ad53b1f4693c69e6472a80dd8
HudsonSchumaker <hudson.schumaker@me.com>
2021-01-17 11:19:49 +0100

    blue print

--- HsCryptoLib/src/br/com/schumaker/crypto/HsEncryptionFile.java
+++ HsCryptoLib/src/br/com/schumaker/crypto/HsEncryptionFile.java
@@ -1,9 +1,20 @@
 package br.com.schumaker.crypto;
 
+import java.io.File;
+
 /**
  *
  * @author Hudson Schumaker
  */
 public class HsEncryptionFile {
     
+    private static final String ALGORITHM = "AES";
+    
+    public void encrypt(final String key, final File inputFile, final File outputFile) throws Exception {
+
+    }
+    
+    public void decrypt(final String key, final File inputFile, final File outputFile) throws Exception {
+
+    }
 }
